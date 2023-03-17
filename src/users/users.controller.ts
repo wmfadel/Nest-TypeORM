5d70f05a1de25c3ab126d62f45d860b3d56ec4a2
wmfadel <fadel24997@gmail.com>
2023-03-17 21:26:14 +0200

    completed user controller CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
 import { UsersService } from './users.service';
 
 @Controller('auth')
@@ -9,5 +19,25 @@ export class UsersController {
   @Post('signup')
   createUser(@Body() body: CreateUserDto) {
     this.userService.create(body.email, body.password);
+  }
+
+  @Get(':id')
+  getUser(@Param('id') id: string) {
+    return this.userService.findOne(parseInt(id));
+  }
+
+  @Get()
+  getAllUsers(@Query('email') email: string) {
+    return this.userService.find(email);
+  }
+
+  @Delete(':id')
+  deleteUser(@Param('id') id: string) {
+    return this.userService.remove(parseInt(id));
+  }
+
+  @Patch(':id')
+  updateUSer(@Param('id') id: string, @Body() attrs: UpdateUserDto) {
+    return this.userService.update(parseInt(id), attrs);
   }
 }
