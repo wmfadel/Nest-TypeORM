1529dabb3a67e7fba2ef1613d0a24c1a0546af5f
wmfadel <fadel24997@gmail.com>
2023-03-25 13:53:04 +0200

    signin users

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { randomBytes, scrypt as _scrypt } from 'crypto';
 import { promisify } from 'util';
 import { User } from './user.entity';
@@ -18,7 +18,22 @@ export class AuthService {
 
     const salt = randomBytes(8).toString('hex');
     const hash = (await scrypt(password, salt, 32)) as Buffer;
-    const hasedPassword = `${salt}.${hash.toString('hex')}`;
-    return await this.usersService.create(email, hasedPassword);
+    const hashedPassword = `${salt}.${hash.toString('hex')}`;
+    return await this.usersService.create(email, hashedPassword);
+  }
+
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    console.log('1',user);
+    if (!user) {
+      throw new NotFoundException('Invalid credentials');
+    }
+    const [salt, hash] = user.password.split('.');
+    const hashedPassword = (await scrypt(password, salt, 32)) as Buffer;
+    if (hashedPassword.toString('hex') !== hash) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    console.log('2',user);
+    return user;
   }
 }
