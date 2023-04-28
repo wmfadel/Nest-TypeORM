8d3aa638803cc6176973266cdbf029bdcb592734
wmfadel <fadel24997@gmail.com>
2023-04-28 22:08:17 +0300

    users controller tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { NotFoundException, Session } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
 describe('UsersController', () => {
   let controller: UsersController;
-
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
+  const users: User[] = [];
+  fakeUsersService = {
+    findOne: (id: number) => {
+      return Promise.resolve({
+        id,
+        email: 'test@email.com',
+        password: '12345',
+      } as User);
+    },
+    find: (email: string) => {
+      return Promise.resolve(users.filter((user) => user.email === email));
+    },
+    remove: (id: number) => {
+      // remove user with id from users array
+      const user = users.find((user) => user.id === id);
+      users.splice(users.indexOf(user), 1);
+      return Promise.resolve(user);
+    },
+    update: (id: number, attrs: Partial<User>) => {
+      const user = users.find((user) => user.id === id);
+      Object.assign(user, attrs);
+      users.splice(users.indexOf(user), 1, user);
+      return Promise.resolve(user);
+    },
+  };
+  fakeAuthService = {
+    signup: (email: string, password: string) => {
+      const user = {
+        id: Math.floor(Math.random() * 99999),
+        email: 'test@email.com',
+        password: '12345',
+      } as User;
+      users.push(user);
+      return Promise.resolve(user);
+    },
+    // signin: (email: string, password: string) => {},
+  };
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +67,24 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('getAllUsers returns a list of users with the given email', async () => {
+    let users = await controller.getAllUsers('test@email.com');
+    expect(users.length).toEqual(0);
+    const session:any = {};
+    await controller.createUser(
+      { email: 'test@email.com', password: '1234' } as CreateUserDto,
+      session,
+    );
+    users = await controller.getAllUsers('test@email.com');
+    expect(users.length).toEqual(1);
+    expect(session.userId).toBeDefined();
+    expect(users[0].email).toEqual('test@email.com');
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.getUser('-1')).rejects.toThrow(NotFoundException);
   });
 });
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,7 +11,7 @@ import {
   Session,
   UseGuards,
 } from '@nestjs/common';
-import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
@@ -19,7 +19,7 @@ import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 import { CurrentUser } from './decorators/current_user.decorator';
-import { AuthGurad } from 'src/guards/auth.guard';
+import { AuthGurad } from '../guards/auth.guard';
 
 @Serialize(UserDto)
 @Controller('auth')
