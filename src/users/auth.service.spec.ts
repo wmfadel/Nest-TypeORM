786024ac60672a01ab0bdc266e2a8cfc49e189ba
wmfadel <fadel24997@gmail.com>
2023-04-28 21:32:13 +0300

    completed auth service tests

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -8,10 +8,20 @@ describe('AuthService', () => {
   let service: AuthService;
   let fakeUsersService: Partial<UsersService>;
   beforeEach(async () => {
+    const users: User[] = [];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find: (email: string) => {
+        return Promise.resolve(users.filter((user) => user.email === email));
+      },
+      create: (email: string, password: string) => {
+        const user = {
+          id: Math.floor(Math.random() * 99999),
+          email,
+          password,
+        } as User;
+        users.push(user);
+        return Promise.resolve(user);
+      },
     };
 
     const module = await Test.createTestingModule({
@@ -40,8 +50,7 @@ describe('AuthService', () => {
   });
 
   it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    await service.signup('asdf@asdf.com', 'asdf');
     await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
       BadRequestException,
     );
@@ -54,20 +63,14 @@ describe('AuthService', () => {
   });
 
   it('throws if an invalid password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        { email: 'asdf@asdf.com', password: 'laskdjf' } as User,
-      ]);
+    await service.signup('laskdjf@alskdfj.com', 'asdf');
     await expect(
       service.signin('laskdjf@alskdfj.com', 'passowrd'),
     ).rejects.toThrow(BadRequestException);
   });
 
-  it('returns a user if correct password is provided', async()=>{
-    fakeUsersService.find = () =>
-    Promise.resolve([
-      { email: 'asdf@asdf.com', password: '9f8cd6bf4fb65ff8.5916ce171c4c79e980cf6f91a4be4cf8e60324da2fb4b18b3fd3afa0bf400a92' } as User,
-    ]);
+  it('returns a user if correct password is provided', async () => {
+    await service.signup('asdf@asdf.com', '12345');
     const user = await service.signin('asdf@asdf.com', '12345');
     expect(user).toBeDefined();
   });
